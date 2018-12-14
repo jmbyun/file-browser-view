dba410153825e64298ffa01f4b41a3d91d59a70f
Jeongmin Byun <jmbyun91@gmail.com>
2018-12-14 11:06:51 +0900

    form a rollup based library dev structure

---
+++ dist/file-browser-view.umd.js
@@ -0,0 +1,15 @@
+
+(function(l, i, v, e) { v = l.createElement(i); v.async = 1; v.src = '//' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; e = l.getElementsByTagName(i)[0]; e.parentNode.insertBefore(v, e)})(document, 'script');
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.FileBrowserView = factory());
+}(this, (function () { 'use strict';
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    return add;
+
+})));
